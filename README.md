c181e2fe3a6d23a3c353cdbb9edc959fbd3cb3e9
GabzBarbosa <gabriella.fkn@gmail.com>
2024-10-13 17:38:16 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeiroprojeto2.0
+Gerenciador de tarefas
+essa é uma melhoria do meu primeiro projeto de gerenciador de tarefas
---
+++ script.js
@@ -0,0 +1,81 @@
+const button = document.querySelector('.button-add-task')
+const input = document.querySelector('.input-task')
+const listaCompleta = document.querySelector('.list-tasks')
+
+let minhaListaDeItens = []
+
+function adicionarNovaTarefa() {
+  minhaListaDeItens.push({
+    tarefa: input.value,
+    concluida: false,
+  })
+
+  input.value = ''
+
+  mostrarTarefas()
+}
+
+function mostrarTarefas() {
+  let novaLi = ''
+
+
+  minhaListaDeItens.forEach((item, posicao) => {
+    novaLi =
+      novaLi +
+      `
+
+        <li class="task ${item.concluida && 'done'}">
+            <img class="imagem" src="./img/concluido.png" alt="check-na-tarefa" onclick="concluirTarefa(${posicao})">
+            <p>${item.tarefa}</p>
+            <img class="imagem" src="./img/lixeira.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${posicao})">
+        </li>
+
+        `
+  })
+
+  listaCompleta.innerHTML = novaLi
+
+  localStorage.setItem('lista', JSON.stringify(minhaListaDeItens))
+}
+
+function concluirTarefa(posicao) {
+  minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
+
+  mostrarTarefas()
+}
+
+function deletarItem(posicao) {
+  minhaListaDeItens.splice(posicao, 1)
+
+  mostrarTarefas()
+}
+
+function recarregarTarefas() {
+  const tarefasDoLocalStorage = localStorage.getItem('lista')
+
+  if (tarefasDoLocalStorage) {
+    minhaListaDeItens = JSON.parse(tarefasDoLocalStorage)
+  }
+
+  mostrarTarefas()
+}
+
+recarregarTarefas()
+button.addEventListener('click', adicionarNovaTarefa)
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+ nextImage();
+},5000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+
+}
