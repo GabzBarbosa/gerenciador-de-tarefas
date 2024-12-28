b94986c4225000868d1288201c1ff22b33ca6ab6
GabzBarbosa <gabriella.fkn@gmail.com>
2024-12-28 10:46:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const $descriptionInput = document.getElementById('description');
 const $priorityInput = document.getElementById('priority');
 const $deadLineInput = document.getElementById('deadLine');
 
+const $todoColumnBody = document.querySelector('#todoColumn .body');
+
+var todoList = [];
+
 function openModal() {
     $modal.style.display = "flex";
 }
@@ -11,10 +15,47 @@ function closeModal() {
     $modal.style.display = "none";
 }
 
+
+function generateCards() {
+    const todoListHtml = todoList.map(function (task) {
+        return `
+       <div class="card">
+        <div class="info">
+        <b>Descrição:</b>
+        <span>${task.description}</span>
+
+        </div>
+
+        <div class="info">
+        <b>Prioridade:</b>
+        <span>${task.priority}</span>
+
+        </div>
+        
+        <div class="info">
+        <b>Prazo:</b>
+        <span>${task.deadline}</span>
+
+        </div>
+    </div>
+        `
+    })
+
+    $todoColumnBody.innerHTML = todoListHtml.join('');
+
+}
+
 function createTask() {
-    console.log($descriptionInput.value);
-    console.log($priorityInput.value);
-    console.log($deadLineInput.value);
+    const newTask = {
+        description: $descriptionInput.value,
+        priority: $priorityInput.value,
+        deadline: $deadLineInput.value,
+    }
+
+    todoList.push(newTask);
 
     closeModal();
+    generateCards();
+
+
 }
